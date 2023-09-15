af229016618b86522ca5c1a6bab4a3e34a4c5668
osman2532 <shiraaziosman@gmail.com>
2023-09-16 04:15:18 +0530

    add functions

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let string = "";
-let buttons = document.querySelectorAll('.button');
+let buttons = document.querySelectorAll('.button,#i');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
-        console.log(e.target)
-        string += e.target.innerHTML;
-        document.querySelector('input').value = string;
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else{console.log(e.target)
+            string += e.target.innerHTML;
+            document.querySelector('input').value = string;
+            }
         })
-    });
+    
+    
+    })
