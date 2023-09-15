fd8baf7081fb0ae090f8dfa6c2638899b3b2d978
osman2532 <shiraaziosman@gmail.com>
2023-09-15 23:40:10 +0530

    js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-lol
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        console.log(e.target)
+        string += e.target.innerHTML;
+        document.querySelector('input').value = string;
+        })
+    });
